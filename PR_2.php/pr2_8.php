0627cdd2189eff833f6391e80655646deb5c35a2
samrat-singh-rajput <asrajputchauhan@gmail.com>
2026-01-21 18:53:30 +0530

    commit

--- PR_2.php/pr2_8.php
+++ PR_2.php/pr2_8.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+   
 </head>
 <body>
-    
+   <form method="post">
+    Columns: <input type="number" name="num" required><br>
+    Row:     <input type="number" name="nums" required><br>
+             <input type="submit">
+   </form> 
+
+   <?php
+   if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $row=$_POST['nums'];
+        $column=$_POST['num'];
+   }
+   ?>
 </body>
 </html>
